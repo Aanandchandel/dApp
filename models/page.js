5d3cdbf41076d4cde115fb37e4334e2a3b7d9684
Jack <aanandchandel2@gmail.com>
2024-05-16 11:57:44 +0530

    home set done

--- models/page.js
+++ models/page.js
@@ -1,12 +1,11 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 
+const pageSchema = new mongoose.Schema({
+  data: { type: String, required: true },
+  email: { type: String, required: true },
+  otp: { type: String, required: true, default: null },
+  password:{type:String,required:true}
+});
 
-const pageSchema=new mongoose.Schema({
-    data:{type:String,
-    required:true},
-    email:{type:String,
-        required:true}
-})
-
-const Page=mongoose.model('Page',pageSchema)
-module.exports=Page;
+const Page = mongoose.model("Page", pageSchema);
+module.exports = Page;
--- routes/introPageRoute.js
+++ routes/introPageRoute.js
@@ -1,59 +1,93 @@
-const express=require("express")
-const introPageRoute=express.Router();
-const Page=require('../models/page');
-introPageRoute.get('/',async(req, res) => {
-    try{
-      const email="jacklinuxnd@gmail.com"
-      const data=await Page.findOne({email})
-      console.log(data)
+const express = require("express");
+const introPageRoute = express.Router();
+const Page = require("../models/page");
+const generateOTP = require("../Funtion/otp.js");
+const mail = require("../Funtion/mail.js");
+
+introPageRoute.get("/", async (req, res) => {
+  try {
+    const email = "jacklinuxnd@gmail.com";
+    const data = await Page.findOne({ email });
+    console.log(data);
     //   res.render('index',{data})
-res.send(data.data)  
-    }catch(err){
-      console.log(err)
-  res.send({err})
-    }
-  
-  });
-
-
-
+    res.send(data.data);
+  } catch (err) {
+    console.log(err);
+    res.send({ err });
+  }
+});
 
 //get the page data HTML
-introPageRoute.get('/pages',async(req,res)=>{
-    const page=await Page.find()
-    console.log("hit the page",page)
+introPageRoute.get("/pages", async (req, res) => {
+  const page = await Page.find();
+  console.log("hit the page", page);
   res.send(page);
-  
-  })
+});
+
+//otp genrate
+introPageRoute.get("/otp", async (req, res) => {
+  try {
+    const otp = generateOTP(6);
+    const data = { user: "jsck", username: "jacklinuxnd@gmail.com", otp };
+    const to = "jacklinuxnd@gmail.com";
+    const mil = await mail("jacklinuxnd@gmail.com", data);
+    const respdata = await Page.findOneAndUpdate({ email: to }, { otp });
+    res.render("reset", { data });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+//reset password
+
+introPageRoute.post("/rpasswort", async (req, res) => {
+  try {
+    const { email, password, otp } = req.body;
+    console.log("getting body ", email, password, otp);
+    const getotp = await Page.findOne({ email });
+    console.log("getting otp", getotp);
+    if (otp == getotp.otp) {
+      const setpassword = await Page.findOneAndUpdate({ email }, { password });
+      console.log("setting password", setpassword);
+      return res.status(200).send({ message: "password changed" });
+    }
+    res.status(400).send({ message: "your otp is invalid" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "server error" });
+  }
+});
 
 //to set the string in page which is HTML
 introPageRoute.post("/set", async (req, res) => {
-    try {
-      const { data, email } = req.body;
-      
-      // Find a page with the given email
-      const existingPage = await Page.findOne({ email });
-  
-      if (existingPage) {
-        // If a page with the email exists, update it
-        const updatedPage = await Page.findOneAndUpdate({ email }, { data, email }, { new: true });
+  try {
+    const { data, email, password } = req.body;
+
+    // Find a page with the given email
+
+    const existingPage = await Page.findOne({ email });
+    if (existingPage) {
+      // If a page with the email exists, update it
+
+      //password chack
+      if (existingPage.password==password) {
+        const updatedPage = await Page.findOneAndUpdate({ email },{ data },{ new: true });
         console.log("Updated page:", updatedPage);
         return res.send({ message: "Page updated", page: updatedPage });
-      } else {
-        // If no page with the email exists, create a new one
-        const newPage = await Page.create({ data, email });
-        console.log("New page created:", newPage);
-        return res.send({ message: "New page created", page: newPage });
+      }else{//if password in not valid
+        return res.status(400).send({message:"password in invaild"})
       }
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: "An error occurred" });
+    } else {
+      // If no page with the email exists, create a new one
+      const newPage = await Page.create({ data, email });
+      console.log("New page created:", newPage);
+      return res.send({ message: "New page created", page: newPage });
     }
-  });
-  
-  
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "An error occurred" });
+  }
+});
 
-
-
-
-module.exports=introPageRoute;
+module.exports = introPageRoute;
